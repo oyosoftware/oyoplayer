21fae248bf6ff86aeb4f81377d2221390e9e5729
oYoSoftware <code@oyosoftware.nl>
2022-01-15 18:02:57 +0100

    Add files via upload

--- getAudio.php
+++ getAudio.php
@@ -7,9 +7,14 @@ $filename = "" . $_GET['source'];
 $pos = mb_strrpos($filename, "/");
 $target = mb_substr($filename, 0, $pos);
 $link = "audio";
+if (file_exists($link)) {
+    rmdir($link);
+    unlink($link);
+}
 symlink($target, $link);
 $filename = $link . mb_substr($filename, $pos);
 
 readfile($filename);
 rmdir($link);
+unlink($link);
 ?>
